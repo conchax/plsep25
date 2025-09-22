c8aa87b659a61441ea087e7759040a9aa280af47
Gonzalo Torres <gonzalo.torres@prepaenlinea.sep.gob.mx>
2025-09-22 12:25:51 -0600

    menu y estlos

--- search.php
+++ search.php
@@ -11,41 +11,35 @@
 
 get_header(); ?>
 <section class="section-w">
-	<div class="container-xxl">
-		<div class="row">
-			
-		</div>
-	</div>
+
 </section>
 <main>
 
 
 <div class="container">
         <h1 class="my-4 text-center">Resultados para: <em><?php the_search_query(); ?></em></h1>
-        <div class="row align-items-center">
+        <div class="row align-items-center justify-content-center">
             <?php 
+            $arg = array(
+            'orderby' => 'ASC',
+            'posts_per_page' => 20
+        );
                 /* $query = new WP_Query(array( "orderby"=>"ID","order"=>"ASC",'post_type' => array( 'post', 'page' ), 'posts_per_page' => 12 ,'paged' => get_query_var('paged') ? get_query_var('paged') : 1) ); */
-            ?>
-            <?php if (have_posts() ) : ?>
+          $searchresult = new WP_Query( $arg );
+          if ($searchresult->have_posts() ) : ?>
                 <?php
-                while (have_posts() ) : the_post(); 
-                       $post_link = get_permalink(); ?>
-                    <div class="col-12 col-md-9 my-2">
+                while ($searchresult->have_posts() ) : $searchresult->the_post(); ?>
+                    <div class="col-lg-8 my-3">
                         <article class="post">
                             <?php
-                            printf( '<h3 class="%s"><a href="%s">%s</a></h3>', 'entry-title color: var(--color-titulos)!important;', esc_url( $post_link ), wp_kses_post( get_the_title() ) );
+                            printf( '<h3 class="%s"><a class="link-green" href="%s">%s</a></h3>', 'entry-title color: var(--color-text)!important;', esc_url( $post_link ), wp_kses_post( get_the_title() ) );
                             echo excerpt('50'); 
                             ?>
                         </article>
+                        <hr class="my-3">
                     </div>
-                    <div class="col-12 col-md-3">
-                        <?php                        
-                            if ( has_post_thumbnail() ) {  
-                                printf( '<a href="%s">%s</a>', esc_url( $post_link ), get_the_post_thumbnail( $post, 'small', ['class' => 'img-fluid'] ) );
-                            }                        
-                        ?>
-                    </div>
-                    <hr>
+                    
+                    
                     <?php
                 endwhile;
                 ?>
@@ -59,11 +53,21 @@ get_header(); ?>
                 <div class="row justify-content-center mt-5">
                     <div class="col-5 ">
                         <div id="contenido">
-                            <form action="<?php echo SITE_URL; ?>" method="get" enctype="multipart/form-data" class="form-only-controls">
+                            <form action="<?php echo SITE_URL; ?>" method="get" enctype="multipart/form-data" class="form-only-controls mt-4">
                                 <div class="input-group">
+                                    <div class="btn" style="border: 1px solid var(--color-border)">
+                                        <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M8.375 15C8.38001 16.3854 8.5535 17.7649 8.89167 19.1084H14.2917V11.2084H8.81667C8.52894 12.4519 8.3808 13.7237 8.375 15Z" fill="#E3E3E3"/>
+                                        <path d="M15.7083 11.2084V19.1084H21.1083C21.4605 17.7667 21.648 16.3871 21.6666 15C21.6635 13.724 21.5182 12.4522 21.2333 11.2084H15.7083Z" fill="#E3E3E3"/>
+                                        <path d="M15.0001 1.66663C12.363 1.66663 9.78514 2.44861 7.59248 3.9137C5.39983 5.37878 3.69086 7.46117 2.68169 9.89751C1.67253 12.3339 1.40848 15.0148 1.92295 17.6012C2.43742 20.1876 3.7073 22.5634 5.572 24.4281C7.4367 26.2928 9.81247 27.5626 12.3989 28.0771C14.9853 28.5916 17.6662 28.3275 20.1025 27.3184C22.5389 26.3092 24.6213 24.6002 26.0864 22.4076C27.5514 20.2149 28.3334 17.637 28.3334 15C28.3334 11.4637 26.9287 8.07235 24.4282 5.57187C21.9277 3.07138 18.5363 1.66663 15.0001 1.66663ZM25.1834 20.5916H22.1668C21.5964 22.3773 20.7531 24.0639 19.6668 25.5916C18.8648 25.9401 18.0262 26.1972 17.1668 26.3583C18.7065 24.6855 19.8914 22.7182 20.6501 20.575H15.7168V26.5333H14.3084V20.5916H9.35009C10.1077 22.7378 11.2927 24.7081 12.8334 26.3833C11.9917 26.224 11.1701 25.9725 10.3834 25.6333C9.29327 24.0928 8.44982 22.3918 7.88342 20.5916H4.81675C4.55069 20.1059 4.31955 19.6019 4.12509 19.0833H7.45842C7.14608 17.7444 6.98397 16.3748 6.97509 15C6.97628 13.7256 7.11034 12.4548 7.37509 11.2083H3.96675C4.14231 10.6913 4.35394 10.1873 4.60009 9.69996H7.77509C8.37141 7.71606 9.29526 5.84583 10.5084 4.16663C11.2784 3.84857 12.0802 3.6139 12.9001 3.46663C11.2417 5.24535 9.99046 7.36387 9.23342 9.67496H14.3084V3.33329H15.7168V9.69996H20.7918C20.0361 7.39257 18.7878 5.27706 17.1334 3.49996C17.9521 3.63648 18.7538 3.85996 19.5251 4.16663C20.7312 5.8337 21.652 7.6895 22.2501 9.65829H25.3918C25.6379 10.1456 25.8495 10.6496 26.0251 11.1666H22.6418C22.9122 12.4265 23.0491 13.7114 23.0501 15C23.0458 16.3829 22.8865 17.7609 22.5751 19.1083H25.9084C25.7021 19.6192 25.4598 20.1149 25.1834 20.5916Z" fill="#E3E3E3"/>
+                                        </svg>
+                                    </div>
                                     <input type="text" name="s" placeholder="Busca en PL-SEP" value="" class="form-control text-left autocomplete" />
-                                    <button type="submit" class="btn btn-default link-menu"><span class="text-hide sr-only">Buscar</span>
-                                            <svg class="iconos-m" width="20" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 54.99 54.96"><defs><style>      .icono-m-1 {        fill: none;        stroke: #E0E7A5;        stroke-linecap: round;        stroke-linejoin: round;        stroke-width: 5px;      }    </style></defs><g data-name="Capa 1"><g><line class="icono-m-1" x1="52.49" y1="52.46" x2="36.62" y2="36.59"></line><circle class="icono-m-1" cx="22.48" cy="22.48" r="19.98"></circle></g></g></svg>
+                                    <button type="submit" class="btn btn-search">
+                                            <svg width="30" height="30" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M15.5253 29.0507C22.9953 29.0507 29.0507 22.9953 29.0507 15.5253C29.0507 8.05537 22.9953 2 15.5253 2C8.05537 2 2 8.05537 2 15.5253C2 22.9953 8.05537 29.0507 15.5253 29.0507Z" stroke="currentColor" stroke-width="4" stroke-linejoin="round"/>
+                                            <path d="M20.0262 10.229C19.4356 9.63725 18.7341 9.16797 17.9617 8.84808C17.1893 8.52818 16.3614 8.36397 15.5254 8.36487C14.6894 8.36397 13.8615 8.52818 13.0891 8.84808C12.3168 9.16797 11.6152 9.63725 11.0247 10.229M25.2493 25.2493L32.0001 32" stroke="currentColor" stroke-width="5" stroke-linecap="round" stroke-linejoin="round"/>
+                                            </svg>
                                     </button>
                                 </div>
                             </form>   
@@ -98,54 +102,7 @@ get_header(); ?>
       </div>
     </div>
 <hr>
-    <div class="container-xxl">
-        <div class="row">
-            <h2 class="my-5">Boletines de
-            <br>Prepa en Línea-SEP</h2>
-        </div>
-    </div>
-    <div class="container-xxl">
-        <div class="row align-items-center">
-        <?php
-        $args = array(
-            'orderby' => 'rand',
-            'posts_per_page' => 3
-        );      
-        $blogsearch = new WP_Query( $args );
-        if ( $blogsearch->have_posts() ) {
-            // Start looping over the query results. 
-            while ( $blogsearch->have_posts() ) {
-                $blogsearch->the_post();
-                ?>
-                <div class="col-12 col-md-4 my-4">
-                    <a class="zoom" href="<?php the_permalink(); ?>">
-                        <div class="card-boletin">
-                        <img src="<?php the_post_thumbnail('large', ['class' => 'img-fluid']); ?> 
-                        
-                        <p class="fecha my-3"><time datetime="<?php the_time('c') ?>" title="<?php the_time('c') ?>"><?php the_time('j \d\e F \d\e Y'); ?></p>
-                        <div>
-                            <p>Continuar leyendo</p>
-                        </div>
-                        </div>
-                    </a>
-
-                    </div>   
-                <?php
-            }
-        }
-        // Restore original post data. 
-        wp_reset_postdata();
-        ?>
-        </div>  
-
-        <div class="row mt-5 pb-5">
-            <div class="col">
-                <div class="text-end">
-                <a class="btn btn-home" href="<?php echo SITE_URL ?>/boletines" role="button">Leer todos los boletines</a>
-                </div>
-            </div>
-        </div>
-    </div>
+    
 
 </main>
 <?php get_footer(); ?>
